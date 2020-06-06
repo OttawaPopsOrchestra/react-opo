0167360598bf879cfc416d2c6b6b03e87422bbce
Kshamina Ghelani <kshamina.ghelani@raventelemetry.com>
2020-06-06 03:09:44 -0400

    rm errors from dateTime utils

--- src/utils/dateTimeUtils.ts
+++ src/utils/dateTimeUtils.ts
@@ -5,11 +5,11 @@ export const concertFrenchStrToDate = (string: string) => {
     return '';
   }
   const parts = string.split(" ");
-  if (parts.length !== 4) throw Error(`Expected 4 parts, got ${parts.length}: "${string}"`);
+  if (parts.length !== 4) return "";
   const [, date, searchMonth, year] = parts;
   const month = months.indexOf(searchMonth.toLowerCase());
   const numYear = parseInt(year, 10);
   const numDate = parseInt(date, 10);
-  if (month < 0) throw Error(`Unknown month: "${searchMonth}"`);
+  if (month < 0) return "";
   return new Date(numYear, month, numDate);
 }
