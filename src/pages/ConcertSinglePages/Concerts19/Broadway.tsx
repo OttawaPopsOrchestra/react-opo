008e2ba2aa7d58d47adeef7d25f62d0e96a0b6f1
Kshamina Ghelani <kshamina.ghelani@raventelemetry.com>
2020-07-11 14:10:40 -0400

    pob

--- src/pages/ConcertSinglePages/Concerts19/Broadway.tsx
+++ src/pages/ConcertSinglePages/Concerts19/Broadway.tsx
@@ -3,24 +3,24 @@ import ConcertSingle from "../../../components/Concerts/ConcertSingle";
 import { useTranslation } from "react-i18next";
 
 export default () => {
-  const { t } = useTranslation(["Concerts", "VGS"]);
+  const { t } = useTranslation(["Concerts", "POB"]);
 
-  const vgsProps = {
-    imgPath: "/img/concerts/concerts-single/2019-2020/VGSHero.jpg",
+  const pobProps = {
+    imgPath: "/img/concerts/concerts-single/2019-2020/POBHero.jpg",
     title: t("title"),
     timeDates: [
       {
         date: t("concert1Date"),
         time: t("concert1Time"),
         ticketLink:
-          "https://meridianshenkman.evenue.net/cgi-bin/ncommerce3/SEGetEventInfo?ticketCode=GS%3ACP%3AC1920%3ACT0320N%3A&linkID=centrepointe&shopperContext",
+          "https://meridianshenkman.evenue.net/cgi-bin/ncommerce3/SEGetEventList?groupCode=CT0516N&linkID=centrepointe&shopperContext=undefined&caller=&appCode=",
       },
     ],
-    price: "$45.75 - $50.75",
+    price: "$25.75 - $50.75",
     buyTickets:
-      "https://meridianshenkman.evenue.net/cgi-bin/ncommerce3/SEGetEventInfo?ticketCode=GS%3ACP%3AC1920%3ACT0320N%3A&linkID=centrepointe&shopperContext",
+      "https://meridianshenkman.evenue.net/cgi-bin/ncommerce3/SEGetEventList?groupCode=CT0516N&linkID=centrepointe&shopperContext=undefined&caller=&appCode=",
     description: t("description"),
-    spotify: "https://open.spotify.com/embed/playlist/1xlIORmvzJLwbRRgsm46gv",
+    spotify: "https://open.spotify.com/embed/playlist/4EBL0jzb36rCwR1EFXT7kq",
   };
-  return <ConcertSingle {...vgsProps} />;
+  return <ConcertSingle {...pobProps} />;
 };
